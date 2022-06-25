65a4670f19f37b331cd6cc8387419086df197d5b
Hyungseock-Yun <bagzein@gmail.com>
2022-06-26 04:17:19 +0900

    - promise - 비동기:동시성 프로그래밍

--- .idea/commonUtils.js
+++ .idea/commonUtils.js
@@ -3,19 +3,28 @@ const log = console.log;
 const curry = f =>
     (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
+const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
+
+const reduceF = (acc, a, f) =>
+    a instanceof Promise ? a.then(
+                              a => f(acc, a), e => e == nop ? acc : Promise.reject(e)
+                            ) : f(acc, a);
+
+const head = iter => go1(take(1, iter), ([h]) => h);
+
 const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  } else {
-    iter = iter[Symbol.iterator]();
-  }
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    acc = f(acc, a);
-  }
-  return acc;
+  if (!iter) return reduce(f, head(iter = acc[Symbol.iterator]()), iter);
+
+  iter = iter[Symbol.iterator]();
+
+  return go1(acc, function recur(acc) {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      acc = reduceF(acc, cur.value, f);
+      if (acc instanceof Promise) return acc.then(recur);
+    }
+    return acc;
+  });
 });
 
 const go = (...args) => reduce((a, f) => f(a), args);
@@ -25,13 +34,21 @@ const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 const take = curry((l, iter) => {
   let res = [];
   iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(a);
-    if (res.length == l) return res;
-  }
-  return res;
+
+  return function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (a instanceof Promise)
+        return a
+                .then(a => (res.push(a), res).length == l ? res : recur())
+                .catch(e => e == nop ? recur() : Promise.reject(e));
+
+      res.push(a);
+      if (res.length == l) return res;
+    }
+    return res;
+  } ();
 });
 
 const takeAll = take(Infinity);
@@ -45,13 +62,17 @@ L.range = function* (l) {
 
 L.map = curry(function* (f, iter) {
   for (const a of iter) {
-    yield f(a);
+    yield go1(a, f);
   }
 });
 
+const nop = Symbol('nop');
+
 L.filter = curry(function* (f, iter) {
   for (const a of iter) {
-    if (f(a)) yield a;
+    const b = go1(a, f);
+    if (b instanceof Promise) yield b.then(b => b ? a : Promise.reject(nop))
+    else if (b) yield a;
   }
 });
 
