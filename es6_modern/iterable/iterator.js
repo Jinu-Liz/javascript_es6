9bc34e749b3dd782b897c02e9607718dd8ae050f
Hyungseock-Yun <bagzein@gmail.com>
2024-01-21 02:38:46 +0900

    - 이터러블: 이터레이터 프로토콜 > Array, Set, Map을 통한 이터레이터 프로토콜

---
+++ es6_modern/iterable/iterator.js
@@ -0,0 +1,41 @@
+/*
+* Array를 통해 알아보기
+* */
+const arr = [1, 2, 3]
+console.log (arr[Symbol.iterator]);
+// arr[Symbol.iterator] = null;
+
+/**
+ * Array에서 Symbol.iterator를 null로 비워버리면 동작하지 않는다.
+ * 따라서, foreach문과 연관이 있을 수 있다고 생각할 수 있다.
+ */
+for (const a of arr) console.log(a);
+
+// iterator에서 next를 하면 다음 값을 얻게 되고, 이는 유지되는 것을 알 수 있다.
+const iterator = arr[Symbol.iterator]();
+console.log(iterator.next());
+for (const a of iterator) console.log(a);   // 앞에서 next를 했기 때문에 2부터 시작함.
+/*
+* Set을 통해 알아보기
+* */
+const set = new Set([1, 2, 3]);
+console.log (set[Symbol.iterator]);
+for (const a of set) console.log(a);
+
+
+/*
+* Map을 통해 알아보기
+* */
+const map = new Map([['a', 1], ['b', 2], ['c', 3],]);
+console.log (map[Symbol.iterator]);
+for (const a of map) console.log(a);
+for (const a of map.keys()) console.log(a);
+for (const a of map.values()) console.log(a);
+for (const a of map.entries()) console.log(a);
+
+/**
+ * ## 이터러블 / 이터레이터 프로토콜
+ * - 이터러블 : 이터레이터를 리턴하는 [Symbol.iterator]() 를 가진 값
+ * - 이터레이터 : { value, done } 객체를 리턴하는 next() 를 가진 값
+ * - 이터러블 / 이터레이터 프로토콜 : 이터러블을 for...of, 전개 연산자 등과 함께 동작하도록한 규약
+ */
