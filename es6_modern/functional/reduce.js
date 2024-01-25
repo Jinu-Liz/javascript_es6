f46f0f401b78144cd0002894bbc52b42807e9183
Hyungseock-Yun <bagzein@gmail.com>
2024-01-25 23:51:02 +0900

    - reduce

--- es6_modern/functional/reduce.js
+++ es6_modern/functional/reduce.js
@@ -1,3 +1,11 @@
+const products = [
+  { name: '반팔티', price: 15000 },
+  { name: '긴팔티', price: 20000},
+  { name: '핸드폰케이스', price: 15000 },
+  { name: '후드티', price: 30000},
+  { name: '바지', price: 25000 }
+];
+
 const nums = [1, 2, 3, 4, 5];
 
 let total = 0;
@@ -28,3 +36,13 @@ console.log(reduce(add, 0, nums));
 console.log(reduce(add, nums));
 
 console.log(add(add(add(add(add(0, 1), 2), 3), 4), 5));   // 위의 로직이 재귀하면서 이런 식으로 됨
+
+/**
+ * 보조함수를 통하여 어떻게 축약할지를 위임하기 때문에,
+ * 복잡한 구조의 데이터를 축약해나감에 어려움이 없다.
+ */
+console.log(reduce(
+    (total_price, products) => total_price + products.price,
+    0,
+    products
+));
