e1301895849c084622a592960a5fc23762c59740
Hyungseock-Yun <bagzein@gmail.com>
2024-01-27 16:30:36 +0900

    - go를 이용하여 가독성 높이기

--- es6_modern/go/go_pipe.js
+++ es6_modern/go/go_pipe.js
@@ -1,3 +1,11 @@
+const products = [
+  { name: '반팔티', price: 15000 },
+  { name: '긴팔티', price: 20000},
+  { name: '핸드폰케이스', price: 15000 },
+  { name: '후드티', price: 30000},
+  { name: '바지', price: 25000 }
+];
+
 const map = (f, iter) => {
   let res = [];
   for (const a of iter) {
@@ -31,8 +39,14 @@ const reduce = (f, acc, iter) => {
   return acc;
 };
 
+const add = (a, b) => a + b;
 
-// 코드를 값으로 다루어 표현력 높이기
+
+
+/**
+ * 코드를 값으로 다루어 표현력 높이기
+ * 함수를 차례로 돌면서 실행시킨다.
+ */
 const go = (...args) => reduce((a, f) => f(a), args);
 
 const pipe = (f, ...fs) => (...args) => go(f(...args), ...fs);
@@ -52,3 +66,24 @@ const func = pipe(
 );
 
 console.log(func(0, 1));
+
+
+
+console.log(
+    reduce(
+        add,
+        map(
+            p => p.price,
+            filter(p => p.price < 20000, products)
+        )
+    )
+);
+
+// go를 이용하여 위 코드의 가독성 향상
+go(
+    products,
+    products => filter(p => p.price < 20000, products),
+    products => map(p => p.price, products),
+    prices => reduce(add, prices),
+    a => console.log(a)
+)
